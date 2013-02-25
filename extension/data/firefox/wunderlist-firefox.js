c3b892e44647dfba038a2be96c7470ed031446df
Raymond May Jr <raymondmay@gmail.com>
2013-02-25 18:06:51 +0100

    cleanup

--- extension/data/firefox/wunderlist-firefox.js
+++ extension/data/firefox/wunderlist-firefox.js
@@ -1,4 +1,4 @@
-(function(WL) {
+(function (WL) {
 
   var overlayId = 'wunderlist_overlay';
 
@@ -73,28 +73,7 @@
     };
 
     showOverlay(postData);
-
-		// var temp = document.createElement('iframe');
-
-		// temp.allowtransparency = 'true';
-		// temp.scrolling = 'no';
-		// temp.id = 'buffer_overlay';
-		// temp.name = 'buffer_overlay';
-		// temp.style.cssText = "border:none;height:100%;width:100%;position:fixed;z-index:99999999;top:0;left:0;opacity:0;display:block;-webkit-transition:opacity 500ms linear;"
-		
-		// var title = encodeURI(document.title || '');
-		// var note = encodeURI(document.url || '');
-
-		// if (!!postData.selection) {
-		// 	note = encodeURI(postData.url + "\n" + postData.selection);
-		// }
-
-		// temp.src = 'http://0.0.0.0:5000/#/extension/add/' + title + '/' + note;
-
-		// temp.onload = function () {
-		// 	temp.style.opacity = 1;
-		// };
-
-		// document.body.appendChild(temp);
 	});
+
+  WL.showOverlay = showOverlay;
 })(window.WL);
--- extension/lib/main.js
+++ extension/lib/main.js
@@ -13,37 +13,66 @@ var mediator    = Cc['@mozilla.org/appshell/window-mediator;1'].getService(Ci.ns
 // Configuration
 var config = {};
 config.plugin = {
-    label: "Remember This Page",
-    icon: {
-        static:         self.data.url('firefox/img/icon-small.png'),
+
+    'label': 'Remember This Page',
+
+    'icon': {
+
+      'static': self.data.url('firefox/img/icon-small.png')
     },
-    version: self.version,
-    menu: {
-        page: {
-            label: "Remember This Page",
-            scripts: [self.data.url('firefox/menu/wunderlist-page.js')]
-        },
-        selection: {
-            label: "Remember Selected Text"
-        }
+
+    'version': self.version,
+
+    'menu': {
+
+      'page': {
+
+        'label': "Remember This Page",
+        'scripts': [self.data.url('firefox/menu/wunderlist-page.js')]
+      },
+
+      'selection': {
+
+        'label': "Remember Selected Text"
+      }
     },
-    overlay: {
 
-        'scripts': [
+    'injected': {
 
-            self.data.url('common/libs/jquery.min.js'),
+      'scripts': [
 
-            self.data.url('common/WL.js'),
-            self.data.url('common/Injectors.js'),
+        self.data.url('common/libs/jquery.min.js'),
 
-            self.data.url('firefox/wunderlist-firefox-port-wrapper.js'),
-            self.data.url('firefox/wunderlist-firefox-data-wrapper.js'),
-            self.data.url('firefox/wunderlist-firefox.js')
-        ],
+        self.data.url('common/WL.js'),
+        self.data.url('common/Injectors.js'),
 
-        'style': self.data.url('common/css/styles.css')
+        self.data.url('firefox/wunderlist-firefox.js')
+      ]
+    },
+
+    'overlay': {
+
+      'scripts': [
+
+        self.data.url('common/libs/jquery.min.js'),
+
+        self.data.url('common/WL.js'),
+        self.data.url('common/Injectors.js'),
+
+        self.data.url('firefox/wunderlist-firefox-port-wrapper.js'),
+        self.data.url('firefox/wunderlist-firefox-data-wrapper.js'),
+        self.data.url('firefox/wunderlist-firefox.js')
+      ]
     }
 };
+
+// Injectors
+var pageMod = require('page-mod').PageMod({
+
+  'include': '*',
+  'contentScriptFile': config.plugin.injected.scripts,
+  'contentStyleFile': require('self').data.url('common/css/styles.css')
+});
 
 // Overlay
 var attachOverlay = function (data, cb) {
@@ -53,28 +82,20 @@ var attachOverlay = function (data, cb) {
     if( ! cb ) cb = function () {};
     if( ! data.embed ) data.embed = {};
 
-    console.log(config.plugin.overlay.style);
-
     var worker = tabs.activeTab.attach({
 
-      'contentStyleFile': config.plugin.overlay.style,
       'contentScriptFile': config.plugin.overlay.scripts
     });
-
-    var pageMod = require('page-mod').PageMod({
-
-      'include': "*",
-      'contentStyleFile': require('self').data.url('common/css/styles.css')
-    });
     
-    worker.port.on('wunderlist_done', function (overlayData) {
-        worker.destroy();
-        cb(overlayData);
-    });
+    // worker.port.on('wunderlist_done', function (overlayData) {
+
+    //     worker.destroy();
+    //     cb(overlayData);
+    // });
 
     // Pass statistic data
-    data.version = config.plugin.version;
-    if( data.embed.placement ) data.placement = data.embed.placement;
+    // data.version = config.plugin.version;
+    // if( data.embed.placement ) data.placement = data.embed.placement;
 
     // Inform overlay that click has occurred
     worker.port.emit("wunderlist_click", data);
@@ -82,97 +103,124 @@ var attachOverlay = function (data, cb) {
 
 // Remember this page
 var button = widgets.Widget({
-    id: 'wunderlist-button',
-    label: config.plugin.label,
-    contentURL: config.plugin.icon.static
+
+  'id': 'wunderlist-button',
+  'label': config.plugin.label,
+  'contentURL': config.plugin.icon['static']
 });
+
 button.on('click', function () {
-    attachOverlay({placement: 'toolbar'}, function() {
-        button.contentURL = config.plugin.icon.static;
-    });
+
+  attachOverlay({placement: 'toolbar'}, function () {
+
+    button.contentURL = config.plugin.icon['static'];
+  });
 });
 
 // Context menu
 var menu = {};
+
 menu.page = cm.Item({
-    label: config.plugin.menu.page.label,
-    image: config.plugin.icon.static,
-    context: cm.PageContext(),
-    contentScriptFile: config.plugin.menu.page.scripts,
-    contentScriptWhen: 'start',
-    onMessage: function (data) {
-        if(data == 'wunderlist_click') {
-            attachOverlay({placement: 'menu-page'});
-        }
+
+  'label': config.plugin.menu.page.label,
+  'image': config.plugin.icon['static'],
+  'context': cm.PageContext(),
+  'contentScriptFile': config.plugin.menu.page.scripts,
+  'contentScriptWhen': 'start',
+  'onMessage': function (data) {
+
+    if (data == 'wunderlist_click') {
+
+      attachOverlay({placement: 'menu-page'});
     }
-});
-menu.selection = cm.Item({
-    label: config.plugin.menu.selection.label,
-    image: config.plugin.icon.static,
-    context: cm.SelectionContext(),
-    contentScriptFile: config.plugin.menu.page.scripts,
-    contentScriptWhen: 'start',
-    onMessage: function (data) {
-        if(data == 'wunderlist_click') {
-            attachOverlay({placement: 'menu-selection'});
-        }
-    }
+  }
 });
 
-var addNavBarButton = function(browserWindow) {
-    var document = browserWindow.document;
-    var navBar = document.getElementById('nav-bar');
-    if (!navBar) {
-        return;
+menu.selection = cm.Item({
+
+  'label': config.plugin.menu.selection.label,
+  'image': config.plugin.icon['static'],
+  'context': cm.SelectionContext(),
+  'contentScriptFile': config.plugin.menu.page.scripts,
+  'contentScriptWhen': 'start',
+  'onMessage': function (data) {
+    
+    if (data == 'wunderlist_click') {
+
+      attachOverlay({placement: 'menu-selection'});
     }
-    var btn = document.createElement('toolbarbutton');
-    btn.setAttribute('id', 'wunderlist-button');
-    btn.setAttribute('type', 'button');
-    // the toolbarbutton-1 class makes it look like a traditional button
-    btn.setAttribute('class', 'toolbarbutton-1');
-    btn.setAttribute('width', 'auto');
-    btn.setAttribute('image', config.plugin.icon.static);
-    // this text will be shown when the toolbar is set to text or text and icons
-    btn.setAttribute('label', config.plugin.label);
-    btn.addEventListener('click', function() {
-        // Go go go
-        attachOverlay({placement: 'toolbar'});
-    }, false);
-    navBar.appendChild(btn);
+  }
+});
+
+var addNavBarButton = function (browserWindow) {
+
+  var document = browserWindow.document;
+  var navBar = document.getElementById('nav-bar');
+
+  if (!navBar) {
+
+      return;
+  }
+
+  var btn = document.createElement('toolbarbutton');
+  btn.setAttribute('id', 'wunderlist-button');
+  btn.setAttribute('type', 'button');
+  // the toolbarbutton-1 class makes it look like a traditional button
+  btn.setAttribute('class', 'toolbarbutton-1');
+  btn.setAttribute('width', 'auto');
+  btn.setAttribute('image', config.plugin.icon['static']);
+  // this text will be shown when the toolbar is set to text or text and icons
+  btn.setAttribute('label', config.plugin.label);
+  btn.addEventListener('click', function() {
+      // Go go go
+      attachOverlay({placement: 'toolbar'});
+  }, false);
+  
+  navBar.appendChild(btn);
 };
 
-var removeNavBarButton = function(browserWindow) {
-    var document = browserWindow.document;
-    var navBar = document.getElementById('nav-bar');
-    var btn = document.getElementById('wunderlist-button');
-    if (navBar && btn) {
-        navBar.removeChild(btn);
-    }
+var removeNavBarButton = function (browserWindow) {
+
+  var document = browserWindow.document;
+  var navBar = document.getElementById('nav-bar');
+  var btn = document.getElementById('wunderlist-button');
+ 
+  if (navBar && btn) {
+
+    navBar.removeChild(btn);
+  }
 };
 
 // Navigation bar icon
 // exports.main is called when extension is installed or re-enabled
-exports.main = function(options, callbacks) {
+exports.main = function (options, callbacks) {
 
-    // for the current window
-    var browserWindow = mediator.getMostRecentWindow('navigator:browser');
-    addNavBarButton(browserWindow);
+  // for the current window
+  var browserWindow = mediator.getMostRecentWindow('navigator:browser');
+  addNavBarButton(browserWindow);
 
-    // handle new windows
-    var windowListener = {
-      onOpenWindow: function(aWindow) {
-        // Wait for the window to finish loading
-        var domWindow = aWindow.QueryInterface(Ci.nsIInterfaceRequestor).getInterface(Ci.nsIDOMWindowInternal || Ci.nsIDOMWindow);
+  // handle new windows
+  var windowListener = {
+
+    'onOpenWindow': function (aWindow) {
+
+      // Wait for the window to finish loading
+      var domWindow = aWindow.QueryInterface(Ci.nsIInterfaceRequestor).getInterface(Ci.nsIDOMWindowInternal || Ci.nsIDOMWindow);
+      addNavBarButton(domWindow);
+      domWindow.addEventListener("load", function () {
+
+        domWindow.removeEventListener("load", arguments.callee, false);
         addNavBarButton(domWindow);
-        domWindow.addEventListener("load", function() {
-          domWindow.removeEventListener("load", arguments.callee, false);
-          addNavBarButton(domWindow);
-        }, false);
-      },
-      onCloseWindow: function(aWindow) {
-        removeNavBarButton(aWindow);
-      },
-      onWindowTitleChange: function(aWindow, aTitle) { }
-    };
-    mediator.addListener(windowListener);
+      }, false);
+    },
+
+    'onCloseWindow': function (aWindow) {
+
+      removeNavBarButton(aWindow);
+    },
+
+    'onWindowTitleChange': function (aWindow, aTitle) {}
+  };
+  
+  mediator.addListener(windowListener);
 };
